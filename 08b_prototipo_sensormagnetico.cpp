dfd37eb08e34094c8acb14c437feed2595b3d1c2
João Gabriel da Cunha Vasconcellos <122104633+JoaoGabrielCunha@users.noreply.github.com>
2025-11-21 16:49:09 -0300

    Add files via upload

---
+++ 08b_prototipo_sensormagnetico.cpp
@@ -0,0 +1,39 @@
+#include <Wire.h>
+#include <QMC5883LCompass.h>
+
+const int SDA_PIN = 15;
+const int SCL_PIN = 16;
+
+QMC5883LCompass compass;
+
+void setup() {
+  Serial.begin(115200);
+  delay(1000);
+
+  // Inicializa I2C nos pinos escolhidos
+  Wire.begin(SDA_PIN, SCL_PIN);
+
+  // Inicializa o magnetômetro
+  compass.init();
+
+  Serial.println("GY-271 iniciado");
+}
+
+void loop() {
+  int x, y, z;
+
+  // Faz leitura
+  compass.read();
+  x = compass.getX();
+  y = compass.getY();
+  z = compass.getZ();
+
+  Serial.print("X: ");
+  Serial.print(x);
+  Serial.print("  Y: ");
+  Serial.print(y);
+  Serial.print("  Z: ");
+  Serial.println(z);
+
+  delay(500);
+}
